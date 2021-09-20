0318860744187005f2abeb8f16482b161e4773eb
Kubi Kubisiak <mckubisiak@gmail.com>
2021-09-20 13:15:08 -0700

    get all test successfully failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -23,6 +23,38 @@ describe('Comment routes', () => {
     });
   });
 
+  it('GET all comments', async () => {
+    const comment = {
+      name: 'Isaac Awing',
+      comment: 'Qbsak is alright',
+      email: 'rebalalliance@ewingcommander.com',
+      date: '2021-08-01T07:00:00.000Z'
+    };
+
+    const comment2 = {
+      name: ' Danerson',
+      comment: 'Kubi has excellent communication',
+      email: 'kubisfriend@friend.com ',
+      date: '2021-09-01T07:00:00.000Z'
+    };
+
+    const comment3 = {
+      name: 'Isaac Awing',
+      comment: 'Qbsak is alright',
+      email: 'rebalalliance@ewingcommander.com',
+      date: '2021-08-11T07:00:00.000Z'
+    };
+
+    await request(app).post('/api/v1/comments').send(comment);
+    await request(app).post('/api/v1/comments').send(comment2);
+    await request(app).post('/api/v1/comments').send(comment3);
+
+    return request(app)
+      .get('/api/v1/comments')
+      .then((res) => {
+        expect(res.body).toEqual([comment, comment2, comment3]);
+      });
+  });
 
   afterAll(() => {
     pool.end();
